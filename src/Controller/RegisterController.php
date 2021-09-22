fdcd7411b8fd71b758680dfe69c1acc630b862fd
matala <dembelematala@gmail.com>
2021-09-22 18:23:17 +0200

    membre utilisateur

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,23 +4,47 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/inscription', name: 'register')]
 
+    private $entityManager;
 
-    public function index(): Response
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
+    }
+
+
+    #[Route('/inscription', name: 'register')]
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
+    {
+
+
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
 
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+
+            $password = $encoder->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($password);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
         return $this->render('register/index.html.twig', [
-            'form' =>$form->createView()
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -5,9 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -16,33 +20,54 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Votre prenom',
-                'require' => false,
                 'attr' => [
-                    'placeholder' =>'Saisissez votre prenom'
-                ]
+                    'placeholder' => 'Saisissez votre prenom'
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
             ])
 
             ->add('lastname', TextType::class, [
                 'label' => 'Votre nom',
-                'require' => false,
                 'attr' => [
-                    'placeholder' =>'Saisissez votre nom'
-                ]
+                    'placeholder' => 'Saisissez votre nom'
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
             ])
 
             ->add('email', EmailType::class, [
                 'label' => 'Votre email',
-                'require' => false,
                 'attr' => [
-                    'placeholder' =>'Saisissez votre email'
-                ]
+                    'placeholder' => 'Saisissez votre email'
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 70
+                ]),
             ])
-            ->add('password', TextType::class, [
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique',
                 'label' => 'Votre mot de passe',
-                'require' => false,
+                'required' => false,
+                'first_options' => ['label' => 'Mot de passe',
                 'attr' => [
-                    'placeholder' =>'Saisissez votre mot de passe'
+                    'placeholder' => 'Saisissez un mot de passe'
                 ]
+            ],
+                'second_options' => ['label' => 'Confirmer votre message',
+                'attr' => [
+                    'placeholder' => 'Confirmer votre mot de passe'
+                ]]
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscire"
             ]);
     }
 
