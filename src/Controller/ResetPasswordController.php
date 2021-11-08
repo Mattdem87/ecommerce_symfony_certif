4dbfa76e1c1dcf75fa845df0c00e1b9c0dce5d5f
matala <dembelematala@gmail.com>
2021-11-08 13:07:03 +0100

    suite et fin mdp oublie

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use App\Classe\Mail;
 use App\Entity\User;
 use App\Entity\ResetPassword;
+use App\Form\ResetPasswordType;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ResetPasswordController extends AbstractController
 {
@@ -18,22 +20,19 @@ class ResetPasswordController extends AbstractController
 
     public function __construct(EntityManagerInterface $entityManager)
     {
-       $this->entityManager = $entityManager; 
+        $this->entityManager = $entityManager;
     }
     #[Route('/mot-de-passe-oublie', name: 'reset_password')]
     public function index(Request $request): Response
     {
-        if ($this->getUser())
-        {
+        if ($this->getUser()) {
             return $this->redirectToRoute('home');
         }
 
-        if ($request->get('email'))
-        {
+        if ($request->get('email')) {
             $user = $this->entityManager->getRepository(User::class)->findOneByEmail($request->get('email'));
-            
-            if ($user)
-            {
+
+            if ($user) {
                 // Enregistrer en base la demande de reset_password
                 $reset_password = new ResetPassword();
                 $reset_password->setUser($user);
@@ -48,33 +47,58 @@ class ResetPasswordController extends AbstractController
                     'token' => $reset_password->getToken()
                 ]);
 
-                $content = "Bonjour ".$user->getFirstname()."<br/>Vous avez demandé à réinitialiser votre mot de passe sur le site ShoeShop.<br/><br/>";
-                $content .= "Merci de bien vouloir cliquer sur le lien suivant pour <a href='".$url."'>mettre à jour votre mot de passe</a>.";
+                $content = "Bonjour " . $user->getFirstname() . "<br/>Vous avez demandé à réinitialiser votre mot de passe sur le site ShoeShop.<br/><br/>";
+                $content .= "Merci de bien vouloir cliquer sur le lien suivant pour <a href='" . $url . "'>mettre à jour votre mot de passe</a>.";
 
                 $mail = new Mail();
-                $mail->send($user->getEmail(), $user->getFirstname().' '.$user->getLastname(), 'Réinialiser votre mot de passe sur ShoeShop', $content);
+                $mail->send($user->getEmail(), $user->getFirstname() . ' ' . $user->getLastname(), 'Réinialiser votre mot de passe sur ShoeShop', $content);
+
+                $this->addFlash('notice', 'Vous allez recevoir un email dans quelques minutes, vous indiquant la procédure à suivre pour le réinitialisation de votre mot de passe.');
+            } else {
+                $this->addFlash('notice', 'Cette adresse email est inconnue');
             }
         }
         return $this->render('reset_password/index.html.twig');
     }
 
     #[Route('/modifier-mon-mot-de-passe/{token}', name: 'update_password')]
-    public function update($token)
+    public function update(Request $request, $token, UserPasswordEncoderInterface $encoder)
     {
         $reset_password = $this->entityManager->getRepository(ResetPassword::class)->findOneByToken($token);
 
-        if (!$reset_password)
-        {
+        if (!$reset_password) {
             return $this->redirectToRoute('reset_password');
         }
 
         $now = new DateTime();
-        if ($now > $reset_password->getCreatedAt()->modify('+ 1 hour'))
-        {
+        if ($now > $reset_password->getCreatedAt()->modify('+ 1 hour')) {
             $this->addFlash('notice', 'Votre demande de mot de passe a expiré. Merci de renouveller votre demande.');
             return $this->redirectToRoute('reset_password');
         }
 
-        dd($reset_password);
+
+        //rendre une vue avec mot de passe et confirmez votre mot de passe.
+        $form = $this->createForm(ResetPasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $new_pwd = $form->get('new_password')->getData();
+
+            // Encodage de mot de passe
+            $password = $encoder->encodePassword($reset_password->getUser(), $new_pwd);
+
+            $reset_password->getUser()->setPassword($password);
+            // Flush en base de donnée
+            $this->entityManager->flush();
+
+
+            // Redirection de l'utilisateur vers la page de conexion
+            $this->addFlash('notice', 'Votre mot de passe a bien été mise à jour.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('reset_password/update.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class ResetPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('new_password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Le mot de passe et la confirmation doivent être identique',
+            'label' => 'Votre nouveau mot de passe',
+            'required' => false,
+            'first_options' => ['label' => 'Mon nouveau mot de passe',
+            'attr' => [
+                'placeholder' => 'Saisissez votre nouveau mot de passe'
+            ]
+        ],
+            'second_options' => ['label' => 'Confirmer votre nouveau mot de passe',
+            'attr' => [
+                'placeholder' => 'Saisissez votre nouveau mot de passe'
+            ]]
+        ])
+
+        ->add('submit', SubmitType::class, [
+            'label' => "Mettre à jour mon mot de passe",
+            'attr' => [
+                'class' => 'btn-block btn-info'
+            ]
+        ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
